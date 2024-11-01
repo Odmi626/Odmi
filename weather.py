d333a2b61c2d35195d7a3c3d37568653fe3a547c
Odmi626 <v21122008@gmail.com>
2024-11-01 20:09:27 +0200

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,36 @@
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from aiogram.types import Message
+import asyncio
+import requests
+
+# Замените 'YOUR_BOT_TOKEN' на токен вашего бота
+API_TOKEN = '7854563455:AAF-qIJ9Kgt6LLCe03YcAGaPe1YppZw7Ekw'
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command('start'))
+async def send_welcome(message: Message):
+    await message.answer("Привіт! Я бот для отримання погоди. Напиши назву міста, щоб дізнатися про поточну погоду.")
+@dp.message()
+async def get_weather(message: Message):
+    city = message.text
+    response = requests.get(f"http://wttr.in/{city}?format=3")
+    
+    if response.status_code == 200:
+        await message.answer(response.text)
+    else:
+        await message.answer("Місто не знайдено. Спробуйте ще раз.")
+
+async def main():
+    dp.message.register(send_welcome, Command(commands=["start"]))
+    dp.message.register(get_weather)
+
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
